07b11f3f0a40f375f1b2c9050fa29e24071c06ea
srivatsaSundar <srivatsas0503@gmail.com>
2024-04-02 20:51:27 +0530

    edited the nav

--- upbeat/src/pages/home/sidebar.tsx
+++ upbeat/src/pages/home/sidebar.tsx
@@ -1,74 +1,38 @@
 import React, { useEffect } from 'react';
-import './sidebar.css'
+import './sidebar.scss'
+import feather from 'feather-icons'
 const Navbar = () => {
-  
+  useEffect(() => {
+    // Initialize Feather Icons after the component is mounted
+    feather.replace();
+  }, []);
+
   return (
     <nav className="navbar">
-      <ul className="navbar-nav">
-      <li className="logo">
-        <a href="#" className="nav-link">
-          <span className="link-text logo-text">Upbeat</span>
-          <svg
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fad"
-            data-icon="angle-double-right"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-            className="svg-inline--fa fa-angle-double-right fa-w-14 fa-5x"
-          >
-            <g className="fa-group">
-              <path
-                fill="currentColor"
-                d="M224 273L88.37 409a23.78 23.78 0 0 1-33.8 0L32 386.36a23.94 23.94 0 0 1 0-33.89l96.13-96.37L32 159.73a23.94 23.94 0 0 1 0-33.89l22.44-22.79a23.78 23.78 0 0 1 33.8 0L223.88 239a23.94 23.94 0 0 1 .1 34z"
-                className="fa-secondary"
-              ></path>
-              <path
-                fill="currentColor"
-                d="M415.89 273L280.34 409a23.77 23.77 0 0 1-33.79 0L224 386.26a23.94 23.94 0 0 1 0-33.89L320.11 256l-96-96.47a23.94 23.94 0 0 1 0-33.89l22.52-22.59a23.77 23.77 0 0 1 33.79 0L416 239a24 24 0 0 1-.11 34z"
-                className="fa-primary"
-              ></path>
-            </g>
-          </svg>
-        </a>
+    <ul className="navbar__menu">
+      <li className="navbar__item">
+        <a href="#" className="navbar__link"><i data-feather="home"></i><span>Home</span></a>
       </li>
-
-      <li className="nav-item">
-        <a href="#" className="nav-link">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-home"><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
-          <span className="link-text">Home</span>
-        </a>
+      <li className="navbar__item">
+        <a href="#" className="navbar__link"><i data-feather="message-square"></i><span>Chat</span></a>        
       </li>
-
-      <li className="nav-item">
-        <a href="#" className="nav-link">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-message-square-more"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/><path d="M8 10h.01"/><path d="M12 10h.01"/><path d="M16 10h.01"/></svg>
-
-          <span className="link-text">Chat</span>
-        </a>
+      <li className="navbar__item">
+        <a href="#" className="navbar__link"><i data-feather="users"></i><span>Customers</span></a>        
       </li>
-
-      <li className="nav-item">
-        <a href="#" className="nav-link">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
-
-          <span className="link-text">Profile</span>
-        </a>
+      <li className="navbar__item">
+        <a href="#" className="navbar__link"><i data-feather="folder"></i><span>Projects</span></a>        
       </li>
-
-      
-
-      <li className="nav-item" id="themeButton">
-        <a href="#" className="nav-link">
-      
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-sun-moon"><path d="M12 8a2.83 2.83 0 0 0 4 4 4 4 0 1 1-4-4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.9 4.9 1.4 1.4"/><path d="m17.7 17.7 1.4 1.4"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.3 17.7-1.4 1.4"/><path d="m19.1 4.9-1.4 1.4"/></svg>
-          <span className="link-text">Theme</span>
-        </a>
+      <li className="navbar__item">
+        <a href="#" className="navbar__link"><i data-feather="archive"></i><span>Resources</span></a>        
       </li>
-      </ul>
-      
-    </nav>
+      <li className="navbar__item">
+        <a href="#" className="navbar__link"><i data-feather="help-circle"></i><span>Help</span></a>        
+      </li>
+      <li className="navbar__item">
+        <a href="#" className="navbar__link"><i data-feather="settings"></i><span>Settings</span></a>        
+      </li>
+    </ul>
+  </nav>
   );
 };
 
